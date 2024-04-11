0524d2b6d06ffbf12303a426b1a3b911db4edddf
Giovanni <louisgiovannir@gmail.com>
2024-04-11 21:49:56 +0300

    feat : create component / Calendar & Member

--- TeamPlaningApp/src/app/app.module.ts
+++ TeamPlaningApp/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MemberComponent } from './components/member/member.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule,
