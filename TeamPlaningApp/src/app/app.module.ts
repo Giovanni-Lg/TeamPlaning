aa7e7433c69e518ea9f2e2259c92dee73a4bc779
Giovanni <louisgiovannir@gmail.com>
2024-04-11 23:28:06 +0300

    feat : UI Base

--- TeamPlaningApp/src/app/app.module.ts
+++ TeamPlaningApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MemberComponent } from './components/member/member.component';
+import {MatChipsModule} from '@angular/material/chips'; 
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MemberComponent } from './components/member/member.component';
     BrowserModule,
     AppRoutingModule,
     FullCalendarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
