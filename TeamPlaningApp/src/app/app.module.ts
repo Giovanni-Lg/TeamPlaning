433399d51ef8cb26dd2054912cc511061567e08e
Giovanni <louisgiovannir@gmail.com>
2024-04-12 12:28:34 +0300

    update : add title team member component

--- TeamPlaningApp/src/app/app.module.ts
+++ TeamPlaningApp/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MemberComponent } from './components/member/member.component';
-import {MatChipsModule} from '@angular/material/chips'; 
-import {HttpClientModule} from '@angular/common/http';
+import { MatChipsModule } from '@angular/material/chips';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {HttpClientModule} from '@angular/common/http';
     FullCalendarModule,
     BrowserAnimationsModule,
     MatChipsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
