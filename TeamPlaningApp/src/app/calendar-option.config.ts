4cf6f08745c092013cbc2c834ad44e912135aced
Giovanni <louisgiovannir@gmail.com>
2024-04-12 19:39:29 +0300

    refactor: calendar option

---
+++ TeamPlaningApp/src/app/calendar-option.config.ts
@@ -0,0 +1,45 @@
+import { Calendar, CalendarOptions, DateSelectArg, EventSourceInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+
+
+export const CALENDAR_OPTIONS_CONFIG: CalendarOptions = {
+    plugins: [
+        interactionPlugin,
+        dayGridPlugin,
+        timeGridPlugin,
+        listPlugin
+    ],
+
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+
+    locales: [
+        {
+            code: 'fr',
+            buttonText: {
+                today: 'Aujourd\'hui',
+                month: 'Mois',
+                week: 'Semaine',
+                day: 'Jour',
+                list: 'Liste'
+            },
+        }
+    ],
+
+    allDayText: "Toute la journée",
+    noEventsText: "Aucun évènement",
+
+
+};
